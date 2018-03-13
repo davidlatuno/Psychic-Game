3dffe5426228b585073f4b4b6d575d7118353462
davidlatuno <dslatuno@gmail.com>
2018-03-13 10:38:34 -0700

    Limited inputs to letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,5 @@
 var letter = "abcdefghijklmnopqrstuvwxyz";
 
-// var randLetter = letter.charAt(Math.random() * (letter.length + 1));
-
 var wins = 0;
 
 var losses = 0;
@@ -14,36 +12,42 @@ window.addEventListener('keydown', key);
 
 function key() {
 
-    var randLetter = letter.charAt(Math.random() * (letter.length + 1));
+    console.log(event.key);
 
-    var userLetter = event.key;
-    if (userLetter === randLetter) {
-        (wins++);
-        guessNum = 10;
-        userGuess.splice(0, userGuess.length);
+    if (letter.includes(event.key)) {
 
-    } else {
-        (guessNum--);
+        var randLetter = letter.charAt(Math.random() * (letter.length + 1));
+
+        var userLetter = event.key;
+        if (userLetter === randLetter) {
+            (wins++);
+            guessNum = 10;
+            userGuess.splice(0, userGuess.length);
+
+        } else {
+            (guessNum--);
 
 
-        if (guessNum === 0) {
-            losses++;
-            guessNum += 10;
+            if (guessNum === 0) {
+                losses++;
+                guessNum += 10;
+            }
+
+            userGuess.push(userLetter);
+
+            if (userGuess.length === 10) {
+                userGuess.splice(0, 10);
+            }
+
         }
 
-        userGuess.push(userLetter);
 
-        if (userGuess.length === 10) {
-            userGuess.splice(0, 10);
-        }
-
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("guessNum").innerHTML = guessNum;
+        document.getElementById("userGuess").innerHTML = userGuess;
     }
 
-
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("losses").innerHTML = losses;
-    document.getElementById("guessNum").innerHTML = guessNum;
-    document.getElementById("userGuess").innerHTML = userGuess;
 }
 
 
