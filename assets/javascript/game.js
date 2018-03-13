88d67eae30263b6a94a4521cb203b123268082ef
davidlatuno <dslatuno@gmail.com>
2018-03-13 12:31:19 -0700

    Added: repeated key press restriction

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,11 +12,10 @@ window.addEventListener('keydown', key);
 
 function key() {
 
-    console.log(event.key);
-
     if (letter.includes(event.key)) {
 
         var randLetter = letter.charAt(Math.random() * (letter.length + 1));
+
 
         var userLetter = event.key;
         if (userLetter === randLetter) {
@@ -24,35 +23,39 @@ function key() {
             guessNum = 10;
             userGuess.splice(0, userGuess.length);
 
-        } else {
-            (guessNum--);
+        } else if (userLetter !== userGuess[userGuess.length - 1]) {
+
+            {
+                (guessNum--);
 
 
-            if (guessNum === 0) {
-                losses++;
-                guessNum += 10;
+                if (guessNum === 0) {
+                    losses++;
+                    guessNum += 10;
+                }
+
+
+
+                userGuess.push(userLetter);
+
             }
-
-            userGuess.push(userLetter);
 
             if (userGuess.length === 10) {
                 userGuess.splice(0, 10);
             }
 
+
+
+            document.getElementById("wins").innerHTML = wins;
+            document.getElementById("losses").innerHTML = losses;
+            document.getElementById("guessNum").innerHTML = guessNum;
+            document.getElementById("userGuess").innerHTML = userGuess;
         }
-
-
-        document.getElementById("wins").innerHTML = wins;
-        document.getElementById("losses").innerHTML = losses;
-        document.getElementById("guessNum").innerHTML = guessNum;
-        document.getElementById("userGuess").innerHTML = userGuess;
     }
-
 }
 
 
-
-
+// if (userLetter !== userGuess[userGuess.length - 1]) 
 
 
 
