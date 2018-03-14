d0b038e8bf8bf4df576adc75c3049ba9733a33d2
davidlatuno <dslatuno@gmail.com>
2018-03-13 17:21:19 -0700

    Refined/organized js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,54 +8,46 @@ var guessNum = 10;
 
 var userGuess = [];
 
-window.addEventListener('keydown', key);
+document.onkeyup = key;
 
 function key() {
 
+    var randLetter = letter.charAt(Math.floor(Math.random() * (letter.length)));
+    
+    var userLetter = event.key;
+
     if (letter.includes(event.key)) {
 
-        var randLetter = letter.charAt(Math.random() * (letter.length + 1));
 
-
-        var userLetter = event.key;
         if (userLetter === randLetter) {
             (wins++);
             guessNum = 10;
             userGuess.splice(0, userGuess.length);
 
-        } else if (userLetter !== userGuess[userGuess.length - 1]) {
+        } else  {
 
-            {
-                (guessNum--);
+            guessNum--;
 
+            userGuess.push(userLetter);
 
-                if (guessNum === 0) {
-                    losses++;
-                    guessNum += 10;
-                }
-
-
-
-                userGuess.push(userLetter);
-
-            }
-
-            if (userGuess.length === 10) {
-                userGuess.splice(0, 10);
-            }
-
-
-
-            document.getElementById("wins").innerHTML = wins;
-            document.getElementById("losses").innerHTML = losses;
-            document.getElementById("guessNum").innerHTML = guessNum;
-            document.getElementById("userGuess").innerHTML = userGuess;
         }
+
+        if (guessNum === 0) {
+            losses++;
+            guessNum += 10;
+        }
+
+        if (userGuess.length === 10) {
+            userGuess.splice(0, 10);
+        }
+
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("guessNum").innerHTML = guessNum;
+        document.getElementById("userGuess").innerHTML = userGuess;
+
     }
 }
-
-
-// if (userLetter !== userGuess[userGuess.length - 1]) 
 
 
 
