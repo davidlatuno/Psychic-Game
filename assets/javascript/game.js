35d754a8f5f2129d0ae094696a98d4f1d19837d9
davidlatuno <dslatuno@gmail.com>
2018-03-13 18:04:54 -0700

    Very repetitive way of truly restricting multiple inputs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,18 +13,18 @@ document.onkeyup = key;
 function key() {
 
     var randLetter = letter.charAt(Math.floor(Math.random() * (letter.length)));
-    
+
     var userLetter = event.key;
 
-    if (letter.includes(event.key)) {
+    if (letter.includes(event.key) && userLetter !== userGuess[userGuess.length - 1] && userLetter !== userGuess[userGuess.length - 2] && userLetter !== userGuess[userGuess.length - 3] && userLetter !== userGuess[userGuess.length - 4] && userLetter !== userGuess[userGuess.length - 5] && userLetter !== userGuess[userGuess.length - 6] && userLetter !== userGuess[userGuess.length - 7] && userLetter !== userGuess[userGuess.length - 8] && userLetter !== userGuess[userGuess.length - 9] && userLetter !== userGuess[userGuess.length - 10]) {
 
 
-        if (userLetter === randLetter) {
+        if (userLetter === randLetter ) {
             (wins++);
             guessNum = 10;
             userGuess.splice(0, userGuess.length);
 
-        } else  {
+        } else {
 
             guessNum--;
 
@@ -45,10 +45,9 @@ function key() {
         document.getElementById("losses").innerHTML = losses;
         document.getElementById("guessNum").innerHTML = guessNum;
         document.getElementById("userGuess").innerHTML = userGuess;
-
     }
-}
 
+}
 
 
 
